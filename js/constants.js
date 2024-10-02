d4e1c5097831d2da48c9d1b01e409dc916aad93d
Matheus Reis <fmatheus.fm@gmail.com>
2024-10-02 15:14:43 -0300

    Att: 'ajuste link'

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const projetos = [
     { id: 1, imagem: 'image/natur.png', titulo: 'NaturShop', link: 'https://github.com/FMafe/NaturShop' },
     { id: 2, imagem: 'image/bazzar.png', titulo: 'Bazzar', link: 'https://github.com/FMafe/bazzar' },
-    { id: 3, imagem: 'image/jogo.png', titulo: 'Jogo do Número', link: 'https://mochila-local.vercel.app/' },
+    { id: 3, imagem: 'image/jogo.png', titulo: 'Jogo do Número', link: 'https://jogo-numero-phi.vercel.app/' },
     { id: 4, imagem: 'image/calc.png', titulo: 'Calculadora', link: 'https://github.com/FMafe/Calc1' },
     
     
