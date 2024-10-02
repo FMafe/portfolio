56b5211ea972f730747ed66de71b55a08e184d9e
Matheus Reis <fmatheus.fm@gmail.com>
2024-10-02 09:53:34 -0300

    ATT: css - add projetos

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,9 @@
 const projetos = [
-    { id: 1, imagem: 'image/mochila.png', titulo: 'Mochila', link: 'https://mochila-local.vercel.app/' },
-    { id: 2, imagem: 'image/seu-projeto-destaque.png', titulo: 'Projeto 2', link: '#' },
-    { id: 2, imagem: 'image/seu-projeto-destaque.png', titulo: 'Projeto 2', link: '#' },
-    { id: 2, imagem: 'image/seu-projeto-destaque.png', titulo: 'Projeto 2', link: '#' }
+    { id: 1, imagem: 'image/natur.png', titulo: 'NaturShop', link: 'https://github.com/FMafe/NaturShop' },
+    { id: 2, imagem: 'image/bazzar.png', titulo: 'Bazzar', link: 'https://github.com/FMafe/bazzar' },
+    { id: 3, imagem: 'image/mochila.png', titulo: 'Mochila', link: 'https://mochila-local.vercel.app/' },
+    { id: 4, imagem: 'image/seu-projeto-destaque.png', titulo: 'Projeto 2', link: '#' },
+    
     
 ];
 
