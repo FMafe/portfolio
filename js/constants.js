24b10439aa70754486a4842e86648f8c6a7ac47b
Matheus Reis <fmatheus.fm@gmail.com>
2024-10-02 15:08:37 -0300

    Att

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,8 @@
 const projetos = [
     { id: 1, imagem: 'image/natur.png', titulo: 'NaturShop', link: 'https://github.com/FMafe/NaturShop' },
     { id: 2, imagem: 'image/bazzar.png', titulo: 'Bazzar', link: 'https://github.com/FMafe/bazzar' },
-    { id: 3, imagem: 'image/mochila.png', titulo: 'Mochila', link: 'https://mochila-local.vercel.app/' },
-    { id: 4, imagem: 'image/seu-projeto-destaque.png', titulo: 'Projeto 2', link: '#' },
+    { id: 3, imagem: 'image/jogo.png', titulo: 'Jogo do Número', link: 'https://mochila-local.vercel.app/' },
+    { id: 4, imagem: 'image/calc.png', titulo: 'Calculadora', link: 'https://github.com/FMafe/Calc1' },
     
     
 ];
