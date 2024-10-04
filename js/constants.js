adf6c389a12294dcb3638f4c1bfd291d63b53a8b
Matheus Reis <fmatheus.fm@gmail.com>
2024-10-04 13:41:22 -0300

    Att: 'Ajustes img e ordens'

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const projetos = [
-    { id: 1, imagem: 'image/natur.png', titulo: 'NaturShop', link: 'https://github.com/FMafe/NaturShop' },
-    { id: 2, imagem: 'image/bazzar.png', titulo: 'Bazzar', link: 'https://github.com/FMafe/bazzar' },
-    { id: 3, imagem: 'image/jogo.png', titulo: 'Jogo do Número', link: 'https://jogo-numero-phi.vercel.app/' },
+    { id: 1, imagem: 'image/jogo.png', titulo: 'Jogo do Número', link: 'https://jogo-numero-phi.vercel.app/' },
+    { id: 2, imagem: 'image/natur.png', titulo: 'NaturShop', link: 'https://github.com/FMafe/NaturShop' },
+    { id: 3, imagem: 'image/bazzar.png', titulo: 'Bazzar', link: 'https://github.com/FMafe/bazzar' },  
     { id: 4, imagem: 'image/calc.png', titulo: 'Calculadora', link: 'https://github.com/FMafe/Calc1' },
     
     
